0f41fb9419f106793cf8f3f89d3201c38d49a47c
SaluteKevin <salute.k@ku.th>
2022-08-11 16:16:14 +0700

    add hardCode tester account for testing

--- src/main/java/ku/cs/app/services/UserDataListHardCodeDataSource.java
+++ src/main/java/ku/cs/app/services/UserDataListHardCodeDataSource.java
@@ -15,31 +15,37 @@ public class UserDataListHardCodeDataSource {
         UserData Put = new UserData();
         UserData Third = new UserData();
         UserData Non = new UserData();
+        UserData Tester = new UserData();
 
         Kevin.setName("Salute");
         Put.setName("Tanapat");
         Third.setName("Sirawit");
         Non.setName("Natchanon");
+        Tester.setName("Name101");
 
         Kevin.setSurname("Khumyunn");
         Put.setSurname("Bumrungthaiworakul");
         Third.setSurname("Laomongkhulchaisri");
         Non.setSurname("Kaewmongkol");
+        Tester.setSurname("Surname101");
 
         Kevin.setUserName("Kevin99008");
         Put.setUserName("putzamonkey");
         Third.setUserName("Sirrtrd3");
         Non.setUserName("auratic_uel");
+        Tester.setUserName("Tester101");
 
         Kevin.setPassword("KevinGG555");
         Put.setPassword("Putza277353");
         Third.setPassword("Tt!1344556");
         Non.setPassword("NonThepZa007");
+        Tester.setPassword("Tester1234");
 
         dataList.addUser(Kevin);
         dataList.addUser(Put);
         dataList.addUser(Third);
         dataList.addUser(Non);
+        dataList.addUser(Tester);
     }
 
     public UserDataList getDataList(){
