5f96c0cb5cb2a712e7d6cbac4050c9243b02608d
SaluteKevin <salute.k@ku.th>
2022-09-29 21:41:12 +0700

    add handle in MainAdminFormController and modify method of MainAdimFormController

--- src/main/java/ku/cs/app/controllers/mainPage/MainAdminFormController.java
+++ src/main/java/ku/cs/app/controllers/mainPage/MainAdminFormController.java
@@ -1,5 +1,7 @@
 package ku.cs.app.controllers.mainPage;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import ku.cs.app.models.Report;
 import ku.cs.app.models.ReportList;
+import ku.cs.app.models.User;
 import ku.cs.app.services.DataSource;
 import ku.cs.app.services.ReportListFileDataSource;
 import com.github.saacsos.FXRouter;
@@ -16,26 +19,32 @@ import com.github.saacsos.FXRouter;
 import java.io.IOException;
 
 public class MainAdminFormController {
-
+    //--------------------------------------------
+    @FXML private ComboBox categoryBox;
+    @FXML private ComboBox timeBox;
+    @FXML private Label nameLabel;
+    @FXML private Label topicLabel;
+    @FXML private Label dateLabel;
+    @FXML private Label categoryLabel;
+    @FXML private Label descriptionLabel;
+    @FXML private ListView<Report> reportListView;
+    //--------------------------------------------
+    private ReportList list;
+    private User user;
+    //--------------------------------------------
     ObservableList<String> categoryList = FXCollections
             .observableArrayList("Environment","Scholarship","Other","Default");
 
     ObservableList<String> timeList = FXCollections
             .observableArrayList("Descending","Ascending","Default");
-
-    @FXML private ListView<Report> reportListView;
-    private ReportList list;
-    private UserData user;
-
-    @FXML private ComboBox categoryBox;
-    @FXML private ComboBox timeBox;
-    @FXML private Label nameLabel;
+    //--------------------------------------------
 
     @FXML
     public void initialize() throws IOException {
         DataSource<ReportList> dataSource = new ReportListFileDataSource("data","report.csv");
         list = dataSource.readData();
         showListView();
+        handleSelectedListView();
 
         categoryBox.setValue("Environment");
         categoryBox.setItems(categoryList);
@@ -58,12 +67,29 @@ public class MainAdminFormController {
         timeBox.setValue("Default");
         timeBox.setItems(timeList);
 
-        user = (UserData) FXRouter.getData();
+        user = (User) FXRouter.getData();
         showUserData();
-//        System.out.println(user.getUserName());
+    }
+    private void handleSelectedListView(){
+        reportListView.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<Report>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Report>
+                                                observable,
+                                        Report oldValue, Report newValue) {
+                        System.out.println(newValue + " is selected");
+                        showSelectedReport(newValue);
+                    }
+                });
     }
     private void showUserData(){
-        nameLabel.setText(user.getUserName());
+        nameLabel.setText(user.getUsername());
+    }
+    private void showSelectedReport(Report report){
+        topicLabel.setText(report.getTopic());
+        dateLabel.setText(report.getDate());
+        categoryLabel.setText("none");
+        descriptionLabel.setText(report.getDescription());
     }
     private void showListView(){
         reportListView.getItems().addAll(list.getAllRpt());
@@ -94,6 +120,16 @@ public class MainAdminFormController {
         } catch (IOException e){
             System.err.println("err ไป project ไม่ได้");
             System.err.println("ให้ตรวจสอบการกําหนด route");
+        }
+    }
+    @FXML
+    public void handleSuspendManagerButton(ActionEvent actionEvent){
+        try {
+            FXRouter.goTo("suspend_form");
+        } catch (IOException e) {
+            System.err.println("err ไป suspendManager ไม่ได้");
+            System.err.println("ให้ตรวจสอบการกําหนด route");
+            e.printStackTrace();
         }
     }
 }
