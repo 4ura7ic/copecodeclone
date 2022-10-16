6c138c514a0531a2129aec7b9da3452b9d08ac59
SaluteKevin <salute.k@ku.th>
2022-10-16 17:44:18 +0700

    update assign report form and controller

--- src/main/java/ku/cs/app/controllers/AssignReportFormController.java
+++ src/main/java/ku/cs/app/controllers/AssignReportFormController.java
@@ -3,15 +3,19 @@ package ku.cs.app.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import com.github.saacsos.FXRouter;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 import ku.cs.app.models.Report;
+import ku.cs.app.models.list.DynamicCategory;
 import ku.cs.app.models.list.ReportList;
 import ku.cs.app.models.User;
 import ku.cs.app.services.DataSource;
-import ku.cs.app.services.ImageDataSource;
+import ku.cs.app.services.DynamicCategoryFileSource;
 import ku.cs.app.services.ReportImageDataSource;
 import ku.cs.app.services.ReportListFileDataSource;
 
@@ -27,13 +31,16 @@ public class AssignReportFormController {
     @FXML private TextField topicTextField;
     @FXML private TextField descriptionTextField;
     @FXML private ImageView image;
+    @FXML private Button cancelButton;
+    @FXML private Rectangle imagePreviewBar;
 
     //-------------------------------------------- private
 
+    private DynamicCategoryFileSource dynamicCategoryFileSource = new DynamicCategoryFileSource("data", "category.csv");
+    private DynamicCategory dynamicCategory = dynamicCategoryFileSource.readData();
     private User user;
     private LocalDateTime date;
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm:ss");
-    private String[] category = {"Education","Environment","Scholarship","Transportation"};
     private ReportImageDataSource getImage;
     private String imageName = "-";
     private String fs = File.separator ;
@@ -41,7 +48,8 @@ public class AssignReportFormController {
 
     public void initialize(){
         user = (User) FXRouter.getData();
-        categoryBox.getItems().addAll(category);
+        categoryBox.getItems().addAll(dynamicCategory.getAllCategory());
+        cancelButton.setVisible(false);
     }
 
     //-------------------------------------------- handle
@@ -64,7 +72,7 @@ public class AssignReportFormController {
                         String formatDate = date.format(formatter);
                         DataSource<ReportList> dataSource = new ReportListFileDataSource("data", "report.csv");
                         ReportList list = dataSource.readData();
-                        list.addReport(new Report(user.getUsername(), topicTextField.getText(), formatDate, categoryBox.getValue(),descriptionTextField.getText(),imageName));
+                        list.addReport(new Report(user.getUsername(), topicTextField.getText(), formatDate, categoryBox.getValue(), descriptionTextField.getText(),imageName));
                         dataSource.writeData(list);
                         FXRouter.goTo("main_user_form");
                     } catch (IOException e) {
@@ -85,11 +93,22 @@ public class AssignReportFormController {
         }
     }
 
+    @FXML public void handleCancelButton(ActionEvent actionEvent){
+        cancelButton.setVisible(false);
+        image.setVisible(false);
+        imagePreviewBar.setVisible(true);
+        imageName = null;
+    }
+
     public void chooseImageButton(ActionEvent event) throws IOException {
         getImage = new ReportImageDataSource();
         imageName = getImage.chooseImage(topicTextField.getText());
         System.out.println(imageName);
-        if(imageName!=null)
+        if(imageName!=null) {
+            imagePreviewBar.setVisible(false);
+            cancelButton.setVisible(true);
+            image.setVisible(true);
             image.setImage(new Image(System.getProperty("user.dir") + fs + "data" + fs + "images" + fs + "reportImage" + fs + imageName));
+        }
     }
 }
